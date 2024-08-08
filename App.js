be4a6afbeda605a7e78ad380934bb87d83ee0ac8
Cleiton Guilherme Rodrigues de Souza <cleiton.souza50@etec.sp.gov.br>
2024-08-07 22:49:15 -0300

    quase pronto, apareceu tela

--- App.js
+++ App.js
@@ -10,9 +10,15 @@ import Button from './components/Button';
 
 import * as ImagePicker from 'expo-image-picker';
 
+import CircleButton from './components/BotaoCirculo';
+
+import IconButton from './components/BotaoIcone';
+
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+
+  const [showAppOptions, setShowAppOptions] = useState(false);
 
   const [selectedImage, setSelectedImage] = useState(null);
 
@@ -24,10 +30,21 @@ export default function App() {
 
     if (!result.canceled) {
       setSelectedImage(result.assets[0].uri);
-      console.log(result);
+      setShowAppOptions(true);
     } else {
       alert('You did not select any image.');
     }
+  };
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {
+    // we will implement this later
+  };
+
+  const onSaveImageAsync = async () => {
+    // we will implement this later
   };
 
   return (
@@ -37,17 +54,35 @@ export default function App() {
         placeholderImageSource={PlaceholderImage} 
         selectedImage={selectedImage}
       />
-    </View>
+      </View>
+      {showAppOptions ? (
+         <View style={styles.optionsContainer}>
+         <View style={styles.optionsRow}>
+           <IconButton icon="refresh" label="Reset" onPress={onReset} />
+           <CircleButton onPress={onAddSticker} />
+           <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+         </View>
+       </View>
+    ):(
     <View style={styles.footerContainer}>
       <Button theme="primary" label="Choose a photo" onPress={pickImageAsync} />
-      <Button label="Use this photo" />
+      <Button label="Use this photo" onPress={() => setShowAppOptions(true)}/>
     </View>
+    )}
     <StatusBar style="auto" />
   </View>
   );
 }
 
 const styles = StyleSheet.create({
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
   footerContainer:{
     flex:1 / 3,
     alignItems: 'center',
